8fa659b9013f063faae82dad7876a9359aedac59
Sergio Gonzalez <sergiogonzalezlopez101@gmail.com>
2023-06-18 00:32:51 -0600

    ruta nombreTareas

--- routes/profesor.js
+++ routes/profesor.js
@@ -36,15 +36,40 @@ profe.get('/curso=:id([0-9]{1,3})', async (req, res, next) => {
       return res.status(200).json({code: 200, message: curso});
   }
   return res.status(404).json({code: 404, message: "Curso no encontrado" });
-}); 
+});
+
+profe.get('/nombreTareas', async (req, res, next) => {
+  const query = "Select titulo_de_la_tarea FROM tareas;";
+  const rows = await db.query(query);
+  return res.status(200).json({ code: 200, message: rows });
+});
+
+profe.post('/crearTarea', async (req, res, next) => {
+  const { titulo, descripcion, valor, fecha_de_vencimiento } = req.body;
+  if (titulo && fecha_de_vencimiento && valor) {
+    let queryUltimoId = `SELECT MAX(id) as max FROM tareas;`;
+    const ultimoId = await db.query(queryUltimoId);
+    let query = `INSERT INTO tareas(id, titulo_de_la_tarea, descripcion, fecha_de_vencimento, valor, id_clase) `;
+    query += `VALUES ('${ultimoId[0].max + 1}', '${titulo}', '${descripcion}', '${fecha_de_vencimiento}', '${valor}', '1');`;
+
+    const rows = await db.query(query);
+
+    if (rows.affectedRows == 1) {
+      return res.status(201).json({ code: 201, message: "Tarea agregado correctamente" });
+    }
+
+    return res.status(500).json({ code: 500, message: "Ocurrio un error" });
+  }
+  return res.status(500).json({ code: 500, message: "Valores incompletos" });
+});
 
 
-const generarCodigo = () => {
+function generarCodigo() {
   const codigo = randomstring.generate({
     length: 6,
     charset: 'alphanumeric'
   });
   return codigo;
-};
+}
 
 module.exports = profe;
